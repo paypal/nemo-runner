46d0338c810d1c47bfaebcb1f54377859bff4112
Matt Edelman <mattedelman@gmail.com>
2017-10-16 11:15:34 -0700

    Fix.nested.suites (#21)
    * add nested suites to test. modify driver kill logic to account for total # of suites
    
    * changelog for nested suites fix
    
    * fix lint errors

--- test/test.js
+++ test/test.js
@@ -2,11 +2,21 @@
 'use strict';
 
 describe('@suite1@suite2@suite3@suite4@', function () {
-    it('may fail a few times', function () {
+    it('may fail a few times1', function () {
         let nemo = this.nemo;
         return nemo.driver.get(nemo.data.baseUrl)
             .then(function () {
                 return nemo.driver.sleep(500);
             });
     });
+    describe('@inner@', function () {
+        it('may fail a few times2', function () {
+            let nemo = this.nemo;
+            return nemo.driver.get(nemo.data.baseUrl)
+              .then(function () {
+                  return nemo.driver.sleep(500);
+              });
+        });
+    });
+
 });
